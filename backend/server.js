be1bb754b3aabd03810e8588bb6a6a3e6084aa00
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-09-24 19:23:21 +0530

    minor clean up

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,17 @@ import { getSFWImages } from "./controllers/sfw.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import { Anime } from "./models/animeSchema.js";
+import { getNSFWImages } from "./controllers/nsfw.js";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/OniChan")
-  .then(() => console.log("DB connected"))
+  .connect(`${process.env.MONGODB_URI}`)
+  .then(() => console.log("Database is connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
@@ -18,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.get("/api/sfw", getSFWImages);
 app.get("/vote/sfw", async (req, res) => {
-  Anime.aggregate([{ $sample: { size: 1 } }])
+  Anime.aggregate([{ $match: { type: "SFW" } }, { $sample: { size: 1 } }])
     .then((result) => {
       // Output the random anime document
       res.json({
@@ -46,7 +50,55 @@ app.get("/vote/sfw/:id", async (req, res) => {
 
 app.get("/top/sfw", async (req, res) => {
   try {
-    const mostRatedAnime = await Anime.find()
+    const mostRatedAnime = await Anime.find({ type: "SFW" })
+      .sort({ rating: -1 })
+      .limit(25)
+      .exec();
+
+    if (mostRatedAnime) {
+      res
+        .status(200)
+        .json({ message: "Most rated anime found!", anime: mostRatedAnime });
+    } else {
+      res.status(404).json({ message: "No anime found!" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error", error: err });
+  }
+});
+
+app.get("/api/nsfw", getNSFWImages);
+app.get("/vote/nsfw", async (req, res) => {
+  Anime.aggregate([{ $match: { type: "NSFW" } }, { $sample: { size: 1 } }])
+    .then((result) => {
+      // Output the random anime document
+      res.json({
+        id: result[0]._id,
+        url: result[0].url,
+        rating: result[0].rating,
+      });
+    })
+    .catch((err) => {
+      console.error(err); // Handle errors
+    });
+});
+app.get("/vote/nsfw/:id", async (req, res) => {
+  const { id } = req.params;
+  const findAnime = await Anime.findOne({ _id: id });
+  if (findAnime) {
+    findAnime.rating = findAnime.rating + 1;
+    findAnime.save();
+    console.log("She is hot 🥵");
+    res.send("");
+  } else {
+    console.log(err);
+  }
+});
+
+app.get("/top/nsfw", async (req, res) => {
+  try {
+    const mostRatedAnime = await Anime.find({ type: "NSFW" })
       .sort({ rating: -1 })
       .limit(25)
       .exec();
@@ -65,5 +117,5 @@ app.get("/top/sfw", async (req, res) => {
 });
 
 app.listen(8000, () => {
-  console.log("Server is running...");
+  console.log(`Server is running ...`);
 });
